6d1c09ba9c9ee66e2bdaf60581d62697b4bcb3d4
nicholasdurham1 <97260398+nicholasdurham1@users.noreply.github.com>
2022-04-14 15:38:14 -0700

    Add files via upload

---
+++ final_project/src/DriveFP.cpp
@@ -0,0 +1,94 @@
+#include <ros/ros.h>
+#include <std_msgs/Float64.h>
+#include <geometry_msgs/TwistStamped.h>
+#include <math.h>
+#include <dynamic_reconfigure/server.h>
+#include <final_project/FinalProjectConfig.h>
+
+// defining node variables
+ros::Publisher CmdVelpub;
+ros::Publisher Steeringpub;
+final_project::FinalProjectConfig  config; 
+
+
+double dis_meters;
+double followDist = 30;
+double vela1;
+double vela2;
+double orginalcommand =23;
+
+
+void printCallback(const ros::TimerEvent & event ){
+ ROS_INFO("velA1 %f", vela1); 
+ ROS_INFO("errordist %f",dis_meters-followDist);
+ //ROS_INFO("velA2 %f", vela2);
+}
+void distCallback(const std_msgs::Float64ConstPtr& msg)
+{dis_meters = msg -> data;
+//ROS_INFO("DataFromDisCall %f", dis_meters);
+
+}
+
+
+void A1Callback(const geometry_msgs::TwistStampedConstPtr& msg)
+{
+  // msg variables
+   vela1 = msg -> twist.linear.x;
+   double anga1 = msg -> twist.angular.z;
+  if (dis_meters > 0){
+  
+// control via distance
+    vela1 = (orginalcommand *0 +vela1*1) + 1*(dis_meters-followDist);
+
+
+if (vela1 > orginalcommand){
+  vela1 = orginalcommand;
+}else if (vela1 < 0){
+  vela1 = 0;
+}
+  }
+    // print to car
+  geometry_msgs::Twist CmdVel;
+  CmdVel.angular.z = anga1;
+  CmdVel.linear.x = vela1;
+ //
+  CmdVelpub.publish(CmdVel);
+}
+void A2Callback(const geometry_msgs::TwistStampedConstPtr& msg)
+{
+  // msg variables
+  vela2 = msg -> twist.linear.x;
+
+}
+
+void reconfig(final_project::FinalProjectConfig& cfg, uint32_t level){
+  ROS_INFO("Current Distance:"); 
+  config = cfg; 
+  followDist = config.distance;
+}
+
+int main(int argc, char **argv)
+{
+  
+  ros::init(argc, argv, "DriveFP");
+  ros::NodeHandle node("~");
+
+  // feed distance
+   
+
+  
+  CmdVelpub = node.advertise<geometry_msgs::Twist>("/a1/cmd_vel",1);
+  ros::Timer writetimer = node.createTimer(ros::Duration(0.001), printCallback);
+  ros::Subscriber sub1 = node.subscribe("/a1/twist", 1, A1Callback);
+  ros::Subscriber sub2 = node.subscribe("/a2/twist", 1, A2Callback);
+  ros::Subscriber sub3 = node.subscribe("/dist", 1, distCallback);
+
+  // dynamic reconfigure 
+  dynamic_reconfigure::Server<final_project::FinalProjectConfig> srv(node);
+  srv.setCallback(boost::bind(reconfig,_1,_2));
+
+
+
+ ros::spin();
+  return 0;
+} 
